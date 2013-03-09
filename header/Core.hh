52b124576243f6c4b9f2587f724c52c49786a14b
florian dell-aiera <dell-a_f@pc-dell-a_f.(none)>
2013-03-09 15:34:27 +0100

    Core

---
+++ header/Core.hh
@@ -0,0 +1,35 @@
+//
+// Core.hh for core in /home/dell-a_f//projets/bmail
+// 
+// Made by florian dell-aiera
+// Login   <dell-a_f@epitech.net>
+// 
+// Started on  Sat Mar  9 15:22:58 2013 florian dell-aiera
+// Last update Sat Mar  9 15:34:13 2013 florian dell-aiera
+//
+
+#ifndef	__CORE_HH__
+#define	__CORE_HH__
+
+#include	<string>
+
+Class		Core: public ICore
+{
+ protected:
+  std::string	_Login;
+  std::string	_Mdp;
+  std::string	_Ip;
+  std::string	_Port;
+  std::string	_Dest;
+  std::string	_Objet;
+  std::string	_Message;
+ public:
+  Core();
+  ~Core();
+  virtual bool	sendMail() const;
+  virtual bool	receiveMail();
+  virtual bool	deleteMail();
+};
+
+
+#endif
